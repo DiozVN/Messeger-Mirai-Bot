5086b5af2b0831f52e0d37b548185227bf0032a9
Crystal <33801881+Crystal7826@users.noreply.github.com>
2021-08-24 21:43:27 -0700

    Update rank.js

--- modules/commands/rank.js
+++ modules/commands/rank.js
@@ -53,7 +53,7 @@ module.exports.makeRankCard = async (data) => {
 //random rankcard by Siêu Đáng Yêu ,png by ngô đức hiển(xin vui lòng giữ credit)
 	const pathCustom = path.resolve(__dirname, "cache", "customrank");
 	var customDir = fs.readdirSync(pathCustom);
-	let random = Math.floor(Math.random() * 22) + 1;
+	let random = Math.floor(Math.random() * 1) + 1;
 	    var dirImage = __root + "/rankcard" + random + ".png";
 
 
